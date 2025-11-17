b2986d2909c5679466ee9368d27f9e913d6ce0ba
friesandmayo <friesandmayo1.0@gmail.com>
2025-11-17 13:44:46 +0100

    added explanations of code options

---
+++ helpers/packages.R
@@ -0,0 +1,38 @@
+# ---------- Loading all packages ------------
+
+require(tinytex) #LaTeX
+require(ggplot2) #plots
+require(AEC) #JP-Renne functions
+require(AER) #NW formula
+require(forecast) #time series forecasting
+require(expm) #matrix exponents
+require(here) #directory finder
+require(dplyr) #data management
+require(tidyr) #also data
+require(lubridate) #data dates management
+require(zoo) #for lagging
+require(jtools) #tables
+require(huxtable) #tables
+require(lmtest) #reg tests
+require(vroom) #for loading data
+require(data.table) #for data filtering
+require(sysid) #for ARMA-X modeling
+require(sandwich) #regression errors
+require(stargazer) #nice reg tables
+require(texreg) #arima tables
+require(future.apply) #parallel computation (speed)
+require(readxl) #for reading excel data
+require(R.matlab) #for loading matlab data (shadowrates)
+require(tseries) #time series tests
+require(eurostat) #eurostat data
+require(fredr) #fredr data
+require(mFilter) #HP filter
+require(knitr) #tables
+require(kableExtra) #tables extra
+require(rdbnomics)
+require(doParallel) #for speed
+require(foreach) #for speedy loops
+require(car) #for MZ hypothesis test
+require(ggridges) #for fancy density plots
+require(strucchange) #Chow & BP tests for structural breaks
+require(transx) #for hamilton filter
