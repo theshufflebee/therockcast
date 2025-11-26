2183435492387344d8399f7e571b55f3c974617d
friesandmayo <friesandmayo1.0@gmail.com>
2025-11-26 10:23:02 +0100

    re-organising JB helper function to go into pseudo out of sample helper set

--- helpers/pseudo_outofsample_tables.R
+++ helpers/pseudo_outofsample_tables.R
@@ -114,6 +114,49 @@ generate_relative_performance_table <- function(dm_results,
 }
 
 
+#--------------------------------------------------------------------------------
+#-----------------                     4                        -----------------
+#--------------------------------------------------------------------------------
 
+
+# This creates a table with the results from the JB test function
+format_jb_table <- function(jb_results_list) {
+  
+  # Start with horizon column from the first model
+  horizons <- jb_results_list[[1]]$horizon
+  
+  # Combine all models
+  combined <- data.frame(horizon = horizons)
+  
+  for(model_name in names(jb_results_list)) {
+    df <- jb_results_list[[model_name]] %>%
+      mutate(
+        display = ifelse(
+          is.na(X_squared),
+          NA,
+          paste0(round(X_squared, 4), " ",
+                 "(", format_p_values_with_stars(p_value), ")")
+        )
+      ) %>%
+      select(horizon, display)
+    
+    # combine by horizon so it is aligned on it
+    combined <- combined %>%
+      left_join(df, by = "horizon")
+    
+    # rename last column to model name
+    names(combined)[ncol(combined)] <- model_name
+  }
+  
+  # print with kable for niece display
+  table_output2 <- kable(combined, 
+                         format = "markdown", 
+                         align = "c", 
+                         caption = "Jarque–Bera Test Results") %>%
+    kable_styling(full_width = FALSE)
+  print(table_output2)
+  if (save_figures) {
+    save_kable(table_output2, paste0("figures/", spec$caption, ".tex"))} 
+}
 
 #--------------------------------------------------------------------------------
