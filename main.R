4c4deb2c2256f89be4b16bcde3c5196bbb58c60d
friesandmayo <friesandmayo1.0@gmail.com>
2025-12-02 13:39:27 +0100

    breaking down

--- main.R
+++ main.R
@@ -27,23 +27,11 @@ package_loader <- function(pkg) {
 package_loader("here")
 
 # --------- iii. Load packages 
+source(here("scripts/00a_packages.R"))
 
 # --------- iv. Load helper functions
-source(here("scripts/packages_script.R"))
-source(here("helpers/raw_plotter.R"))
-source(here("helpers/stationarity_tables.R"))
-source(here("helpers/stationarity_tests.R"))
-source(here("helpers/taylor_tables.R"))
-source(here("helpers/struct_breaks_tables.R"))
-source(here("helpers/roll_TR_plotter.R"))
-source(here("helpers/pseudo_outofsample_tables.R"))
-source(here("helpers/pseudo_outofsample_plots.R"))
-source(here("helpers/actual_forecast_displays.R"))
-source(here("helpers/actual_forecast_estimator.R"))
-source(here("helpers/auto_ARIMA_replic.R"))
-source(here("helpers/pseudo_outofsample_tests.R"))
-source(here("helpers/roll_TR_estimator.R"))
-source(here("helpers/struct_breaks_tests.R"))
+source(here("scripts/00b_helpers.R"))
+
 
 # --------- v. Load API keys for data (only FRED required)
 fredr_set_key(Sys.getenv("FRED_API_KEY"))
---
+++ scripts/00b_helpers.R
@@ -0,0 +1,27 @@
+#--------------------------------------------------------------------------------
+
+#           This script loads all helper functions
+
+#--------------------------------------------------------------------------------
+
+
+source(here("helpers/raw_plotter.R"))
+source(here("helpers/stationarity_tables.R"))
+source(here("helpers/stationarity_tests.R"))
+source(here("helpers/taylor_tables.R"))
+source(here("helpers/struct_breaks_tables.R"))
+source(here("helpers/roll_TR_plotter.R"))
+source(here("helpers/pseudo_outofsample_tables.R"))
+source(here("helpers/pseudo_outofsample_plots.R"))
+source(here("helpers/actual_forecast_displays.R"))
+source(here("helpers/actual_forecast_estimator.R"))
+source(here("helpers/auto_ARIMA_replic.R"))
+source(here("helpers/pseudo_outofsample_tests.R"))
+source(here("helpers/roll_TR_estimator.R"))
+source(here("helpers/struct_breaks_tests.R"))
+
+
+
+
+
+#--------------------------------------------------------------------------------
